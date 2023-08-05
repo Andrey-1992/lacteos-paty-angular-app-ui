147145228bf9cf35ecc2f1113bdd34df9c618250
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-05 17:23:43 -0600

    create-new-compoentn-for-admin-menu-roles

---
+++ src/app/Factory/FactoryAdmin/factory-admin-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factory-admin-login',
+  templateUrl: './factory-admin-menu.component.html',
+  styleUrls: ['./factory-admin-menu.component.css']
+})
+export class FactoryAdminMenu {
+
+}
--- src/app/app-routing-factory.module.ts
+++ src/app/app-routing-factory.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FactoryUserSelection } from "./Factory/factory-user-selection.component";
 import { FactoryAdminLogin } from "./Factory/FactoryAdmin/factory-admin-login.component";
+import { FactoryAdminMenu } from "./Factory/FactoryAdmin/factory-admin-menu.component";
 import { ProductionVsFactoryComponent } from "./production-vs-factory/production-vs-factory.component";
 
 const appRoutes: Routes = [
   {path:'', redirectTo: '/production-vs-factory', pathMatch:'full'},
   {path: 'production-vs-factory', component: ProductionVsFactoryComponent},
   {path: 'factory-user-selection', component: FactoryUserSelection},
-  {path: 'factory-admin-login', component: FactoryAdminLogin}
+  {path: 'factory-admin-login', component: FactoryAdminLogin},
+  {path: 'factory-admin', component: FactoryAdminMenu}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingFactoryModule {};
-// FactoryAdminLogin
+// FactoryAdminMenu
