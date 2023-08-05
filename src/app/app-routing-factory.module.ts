5d797efc755006fe8d0394204a1831b34b4ccf11
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-05 14:21:17 -0600

    add-routing-module-for-both-paths

--- src/app/app-routing-factory.module.ts
+++ src/app/app-routing-factory.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { FactoryUserSelection } from "./Factory/factory-user-selection.component";
+import { ProductionVsFactoryComponent } from "./production-vs-factory/production-vs-factory.component";
 
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/production-vs-factory'},
+  {path: 'production-vs-factory', component: ProductionVsFactoryComponent},
+  {path: 'factory-user-selection', component: FactoryUserSelection}
+];
 @NgModule({
 
 })
 export class AppRoutingFactoryModule {
 
 }
+// ProductionVsFactoryComponent,
+// FactoryUserSelection
