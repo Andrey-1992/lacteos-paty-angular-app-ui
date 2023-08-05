68c1e1433a34d179a768a1eed59b56546930ea1f
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-05 17:12:04 -0600

    create-routing-paths-for-admin-login-version

--- src/app/app-routing-factory.module.ts
+++ src/app/app-routing-factory.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FactoryUserSelection } from "./Factory/factory-user-selection.component";
+import { FactoryAdminLogin } from "./Factory/FactoryAdmin/factory-admin-login.component";
 import { ProductionVsFactoryComponent } from "./production-vs-factory/production-vs-factory.component";
 
 const appRoutes: Routes = [
   {path:'', redirectTo: '/production-vs-factory', pathMatch:'full'},
   {path: 'production-vs-factory', component: ProductionVsFactoryComponent},
-  {path: 'factory-user-selection', component: FactoryUserSelection}
+  {path: 'factory-user-selection', component: FactoryUserSelection},
+  {path: 'factory-admin-login', component: FactoryAdminLogin}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingFactoryModule {
-
-}
+export class AppRoutingFactoryModule {};
+// FactoryAdminLogin
