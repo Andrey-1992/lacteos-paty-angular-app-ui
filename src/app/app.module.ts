7ee8772d74b5d0b920ece49281f3f68f13424b86
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-05 14:09:49 -0600

    create-component-to-handle-factory-user-selections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FactoryUserSelection } from './Factory/factory-user-selection.component';
 import { ProductionVsFactoryComponent } from './production-vs-factory/production-vs-factory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductionVsFactoryComponent
+    ProductionVsFactoryComponent,
+    FactoryUserSelection
   ],
   imports: [
     BrowserModule
