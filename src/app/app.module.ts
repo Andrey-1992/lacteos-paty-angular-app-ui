a9afbfe7687311dcead94cd57829a27bc2b50342
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-05 17:40:44 -0600

    create-imports-for-ngmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { AppRoutingFactoryModule } from './app-routing-factory.module';
 
 import { AppComponent } from './app.component';
 import { FactoryUserSelection } from './Factory/factory-user-selection.component';
+import { FactoryAdminLogin } from './Factory/FactoryAdmin/factory-admin-login.component';
+import { FactoryAdminMenu } from './Factory/FactoryAdmin/factory-admin-menu.component';
 import { ProductionVsFactoryComponent } from './production-vs-factory/production-vs-factory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductionVsFactoryComponent,
-    FactoryUserSelection
+    FactoryUserSelection,
+    FactoryAdminLogin,
+    FactoryAdminMenu
   ],
   imports: [
     BrowserModule,
