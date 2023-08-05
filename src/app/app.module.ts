89dcc2dbc9438b2e498222b91c46e49a0ebe067e
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-05 13:47:22 -0600

    create-prod-vs-fabrica-component?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductionVsFactoryComponent } from './production-vs-factory/production-vs-factory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductionVsFactoryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/production-vs-factory/production-vs-factory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-production-vs-factory',
+  templateUrl: './production-vs-factory.component.html',
+  styleUrls: ['./production-vs-factory.component.css']
+})
+export class ProductionVsFactoryComponent {
+
+}
