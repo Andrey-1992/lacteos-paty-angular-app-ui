82981dd8ea486e85164f3ce389463720eebc23f9
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-05 16:55:56 -0600

    fix-routes-forcomponents

---
+++ src/app/Factory/FactoryAdmin/factory-admin-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factory-user-selection',
+  templateUrl: './factory-user-selection.component.html',
+  styleUrls: ['./factory-user-selection.component.css']
+})
+export class FactoryUserSelection {
+
+}
