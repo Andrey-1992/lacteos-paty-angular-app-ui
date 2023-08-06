1a0dad57dd602e8664f42ef7240eb3c4ff8de4d2
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-06 00:54:01 -0600

    fix-routing-methods

--- src/app/app-routing-factory.module.ts
+++ src/app/app-routing-factory.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { FactoryAdminMenu } from "./Factory/factory-admin-menu.component";
 import { FactoryUserSelection } from "./Factory/factory-user-selection.component";
 import { FactoryAdminLogin } from "./Factory/FactoryAdmin/factory-admin-login.component";
+import { FactoryProdInputsMenu } from "./Factory/FactoryProdInputs/factory-prod-inputs-menu.component";
 import { FactoryProdMenu } from "./Factory/FactoryProdMenu/factory-prod-menu.component";
 import { ProductionVsFactoryComponent } from "./production-vs-factory/production-vs-factory.component";
 
@@ -12,7 +13,8 @@ const appRoutes: Routes = [
   {path: 'factory-user-selection', component: FactoryUserSelection},
   {path: 'factory-admin-login', component: FactoryAdminLogin},
   {path: 'factory-menu', component: FactoryAdminMenu},
-  {path: 'factory-prod', component: FactoryProdMenu}
+  {path: 'factory-prod', component: FactoryProdMenu},
+  {path: 'factory-prod-inputs-menu', component: FactoryProdInputsMenu}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
