97f44fd09ed46582f9c351f9dfe6edca6a85b1aa
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-07 13:42:40 -0600

    Add-elemtns-to-cheese-data-model

--- src/app/Factory/FactoryShared/CheeseDataStructure/cheese-data-structure.model.ts
+++ src/app/Factory/FactoryShared/CheeseDataStructure/cheese-data-structure.model.ts
@@ -1,6 +1,18 @@
 export class CheeseDataStructureModel {
   constructor(
-    public quesoname: string, 
+    public id?: number,
+    public quesoname?: string,
+    public quesostatus?: string,
+    public quesoavailability?: boolean,
+    public nopieces?: number,
+    public nopiecessalida?: number,
+    public weigth?: number,
+    public weigthcurrent?: number,
+    public holladas?: number,
+    public datein?: string,
+    public dateout?: string ,
+    public price?: number,
     public approvepord?: string,
+    public lotenum?: string
   ) {}
-}
+};
