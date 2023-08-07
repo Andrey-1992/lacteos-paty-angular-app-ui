4efef12fed139f85f2e34043a03aa58d7c249978
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-07 13:23:59 -0600

    create-template-for0summing-cheese-data

--- src/app/Factory/FactoryProdMenu/factory-prod-menu.component.ts
+++ src/app/Factory/FactoryProdMenu/factory-prod-menu.component.ts
@@ -9,5 +9,72 @@ export class FactoryProdMenu {
   cheeseUrl: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShezzrcP_kQ2aqlw0AgJqSi2UWDekCbm3JNQ&usqp=CAU";
   dateObj = new Date();
   dateArray = this.dateObj.toString().split(' ');
-  todaysDate = this.dateArray.splice(1, 3).join(' / ')
+  todaysDate = this.dateArray.splice(1, 3).join(' / ');
+
+  baralosoCurrentStorage: number = 0;
+  comitecoCurrentStorage: number = 0;
+  comitecoAACurrentStorage: number = 0;
+  comitecoBArrentStorage: number = 0;
+
+  filterCheeseDataByDate = (data:any): void => {
+    let baraloso = 0;
+    let comiteco = 0;
+    let comitecoAa = 0;
+    let comitecoBa = 0;
+  }
+
+  // const [ baralosoStorage, setBaralosoStorage ] = useState<number>(0);
+  // const [ comitecoStorage, setComitecoStorage ] = useState<number>(0);
+  // const [ comitecoAaStorage, setComitecoAaStorage ] = useState<number>(0);
+  // const [ comitecoBaStorage, setComitecoBaStorage ] = useState<number>(0);
+  // const [ florAaStorage, setFlorAaStorage ] = useState<number>(0);
+  // const [ florBaStorage, setFlorBaStorage ] = useState<number>(0);
+
+
+  // const filterDataByDate = (data:any): void => {
+  //   let baraloso = 0;
+  //   let comiteco = 0;
+  //   let comitecoAa = 0;
+  //   let comitecoBa = 0;
+  //   let florAa = 0;
+  //   let florBa = 0;
+
+  //   const dataFiltered = data.map((dataRecords: any) => {
+  //     if (dataRecords.quesoname === 'Baraloso' && dataRecords.approvepord === 'Verdadero') {
+  //       baraloso += dataRecords.nopieces
+  //     }
+  //     if (dataRecords.quesoname === 'Comiteco' && dataRecords.approvepord === 'Verdadero') {
+  //       comiteco += dataRecords.nopieces
+  //     }
+  //     if (dataRecords.quesoname === 'Comiteco AA' && dataRecords.approvepord === 'Verdadero') {
+  //       comitecoAa += dataRecords.nopieces
+  //     }
+  //     if (dataRecords.quesoname === 'Comiteco BA' && dataRecords.approvepord === 'Verdadero') {
+  //       comitecoBa += dataRecords.nopieces
+  //     }
+  //     if (dataRecords.quesoname === 'Flor AA' && dataRecords.approvepord === 'Verdadero') {
+  //       florAa += dataRecords.nopieces
+  //     }
+  //     if (dataRecords.quesoname === 'Flor BA' && dataRecords.approvepord === 'Verdadero') {
+  //       florBa += dataRecords.nopieces
+  //     }
+  //   })
+  //   setBaralosoStorage(baraloso);
+  //   setComitecoStorage(comiteco);
+  //   setComitecoAaStorage(comitecoAa);
+  //   setComitecoBaStorage(comitecoBa);
+  //   setFlorAaStorage(florAa);
+  //   setFlorBaStorage(florBa);
+  // }
+
+  // const makeFetch = (): void => {
+  //   fetchAllProdRecords()
+  //   .then(data => filterDataByDate(data))
+  //   .catch(error => console.log(error))
+  // }
+
+  // useEffect(() => {
+  //   makeFetch()
+  // }, [])
+
 }
