42f046764a591c4dc7939d04fb09d139eb02770e
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-07 13:19:50 -0600

    add-date-logic-for-array

--- src/app/Factory/FactoryProdMenu/factory-prod-menu.component.ts
+++ src/app/Factory/FactoryProdMenu/factory-prod-menu.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./factory-prod-menu.component.css']
 })
 export class FactoryProdMenu {
-  cheeseUrl: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShezzrcP_kQ2aqlw0AgJqSi2UWDekCbm3JNQ&usqp=CAU"
+  cheeseUrl: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShezzrcP_kQ2aqlw0AgJqSi2UWDekCbm3JNQ&usqp=CAU";
+  dateObj = new Date();
+  dateArray = this.dateObj.toString().split(' ');
+  todaysDate = this.dateArray.splice(1, 3).join(' / ')
 }
