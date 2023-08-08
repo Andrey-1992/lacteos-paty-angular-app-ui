cdb6224045f531a60efa6a0b426b2ab799a92bcb
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-08 08:04:35 -0600

    add-routing-for-new-component

--- src/app/Factory/FactoryShared/CheeseDataStructure/create-cheese-data-input.ts
+++ src/app/Factory/FactoryShared/CheeseDataStructure/create-cheese-data-input.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-cheese-input',
-  templateUrl: './create-cheese-data-input.html',
-  styleUrls: ['./create-cheese-data-input.css']
+  templateUrl: './create-cheese-data-input.component.html',
+  styleUrls: ['./create-cheese-data-input.component.css']
 })
 export class CreateCheeseDataInput {
 
