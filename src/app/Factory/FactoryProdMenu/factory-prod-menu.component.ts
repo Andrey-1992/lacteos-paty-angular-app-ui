f8b74a13d5f059b8a55e87c703271e8e53415a8e
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-11 14:03:15 -0600

    clean-up-unused-code

--- src/app/Factory/FactoryProdMenu/factory-prod-menu.component.ts
+++ src/app/Factory/FactoryProdMenu/factory-prod-menu.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CheeseDataStructureModel } from '../FactoryShared/CheeseDataStructure/cheese-data-structure.model';
-import { DataStorageService } from '../FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service';
-import { map } from "rxjs/operators";
 import { DataHandlingService } from '../FactoryShared/FactoryProdDatastorage/factory-prod-data-handling.service';
 
 
@@ -11,56 +8,21 @@ import { DataHandlingService } from '../FactoryShared/FactoryProdDatastorage/fac
   styleUrls: ['./factory-prod-menu.component.css']
 })
 export class FactoryProdMenu implements OnInit{
-  constructor(private dataHandlingService: DataHandlingService ,private dataStorage: DataStorageService) {}
-
+  
   filterData: {cheeseName: '', piecesNo: 0}[] = [];
   fetchedData: boolean = false;
+
   dateObj = new Date();
   dateArray = this.dateObj.toString().split(' ');
   todaysDate = this.dateArray.splice(1, 3).join(' / ');
 
+  constructor(private dataHandlingService: DataHandlingService) {}
+
   ngOnInit(): void {
-    // this.dataStorage.getCheeseRecords()
-    // .subscribe(resData => {
-    //   this.filterCheeseDataByDate(resData);
-    // })
     this.filterData = this.dataHandlingService.filterCheeseDataByDate();
     if (this.filterData.length > 0) {
       this.fetchedData = true;
     }
-    console.log(this.filterData);
+    // console.log(this.filterData);
   };
-
-  // baralosoCurrentStorage: number = 0;
-  // comitecoCurrentStorage: number = 0;
-  // comitecoAACurrentStorage: number = 0;
-  // comitecoBACurrentStorage: number = 0;
-
-  // filterCheeseDataByDate = (cheeseData:CheeseDataStructureModel[]): void => {
-  //   let baraloso = 0;
-  //   let comiteco = 0;
-  //   let comitecoAa = 0;
-  //   let comitecoBa = 0;
-  //   // console.log(cheeseData)
-
-  //   cheeseData.map((dataRecords: CheeseDataStructureModel) => {
-  //     if (dataRecords.cheeseName === 'Baraloso' && dataRecords.approveProd === 'Verdadero') {
-  //       baraloso += dataRecords.piecesNo
-  //     }
-  //     if (dataRecords.cheeseName === 'Comiteco' && dataRecords.approveProd === 'Verdadero') {
-  //       comiteco += dataRecords.piecesNo
-  //     }
-  //     if (dataRecords.cheeseName === 'Comiteco AA' && dataRecords.approveProd === 'Verdadero') {
-  //       comitecoAa += dataRecords.piecesNo
-  //     }
-  //     if (dataRecords.cheeseName === 'Comiteco BA' && dataRecords.approveProd === 'Verdadero') {
-  //       comitecoBa += dataRecords.piecesNo
-  //     }
-  //   })
-
-  //   this. baralosoCurrentStorage = baraloso;
-  //   this.comitecoCurrentStorage = comiteco;
-  //   this.comitecoAACurrentStorage = comitecoAa;
-  //   this.comitecoBACurrentStorage = comitecoBa;
-  // }
 }
