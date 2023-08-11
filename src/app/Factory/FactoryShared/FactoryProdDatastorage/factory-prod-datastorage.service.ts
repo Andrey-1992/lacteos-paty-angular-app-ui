ed8a9ff347e0a737106785638cd3df5b464290bc
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-11 00:02:25 -0600

    import=data-cheese-strcuture

--- src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
+++ src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { CheeseDataStructureModel } from "../CheeseDataStructure/cheese-data-structure.model";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
 
-  storeCheeseRecord() {
-    
+  storeCheeseRecord(cheeseData: CheeseDataStructureModel) {
+
   }
 }
