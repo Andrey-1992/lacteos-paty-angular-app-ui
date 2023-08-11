faa9eee76a3609971038a715ca185b2fb6ed29db
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-10 23:59:36 -0600

    clean-up-function

--- src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
+++ src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
@@ -4,9 +4,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
 
-  storeRecipes() {
-    const recipes = this.recipeService.getRecipes();
-    this.http.put('https://udemy-recipe-course-f2d4f-default-rtdb.firebaseio.com/recipes.json', recipes)
-      .subscribe(response => { console.log(response) })
+  storeCheeseRecord() {
+    
   }
 }
