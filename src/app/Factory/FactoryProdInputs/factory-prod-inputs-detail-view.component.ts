008ad6b95d57c098a59e88b8de14ea8c1b06bc7d
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-13 16:57:15 -0600

    add-ngClass-styling

--- src/app/Factory/FactoryProdInputs/factory-prod-inputs-detail-view.component.ts
+++ src/app/Factory/FactoryProdInputs/factory-prod-inputs-detail-view.component.ts
@@ -12,16 +12,29 @@ export class FactoryProdInputsDetailView  implements OnInit{
   @Input() filteredRecords: CheeseDataStructureModel = {};
   @Output() overview = new EventEmitter<boolean>();
   cleanedDate:string = '';
-  // @Output() selectedRecord = new EventEmitter<CheeseDataStructureModel>();
+
+  cheeseCategory = [
+    {Name:'Selecione Categoria', Value:''}, 
+    {Name:'Baraloso', Value:'Baraloso'}, {Name:'Comiteco', Value:'Comiteco'}, 
+    {Name:'Comiteco BA', Value:'Comiteco BA'}, {Name:'Comiteco AA', Value:'Comiteco AA'}
+  ];
+
+  refrigerationCategory = [
+    {Name:'Selecione Refrigeracion', Value:''}, {Name:'Grande', Value:'Grande'}, 
+    {Name:'Mediana', Value:'Mediana'}, {Name:'Chica', Value:'Chica'}
+  ];
+
+  cheeseStatus = [
+    {Name:'Falso', Value:'Falso'}, {Name:'Verdadero', Value:'Verdadero'}
+  ];
+
 
   ngOnInit(): void {
-    // console.log(this.filteredRecords)
     this.cleanedDate = this.filteredRecords.dateIn.slice(0, 10).split('-').join(' / ')
   }
 
   sendViewStatus = ():void => {
     event.preventDefault()
     this.overview.emit(false);
-    // this.selectedRecord.emit(this.filteredRecords);
   }
 };
