5f0f4b176ef5ba3e5b701a372b8be58f631d6b32
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-13 13:13:31 -0600

    change-array-of0bjects

--- src/app/Factory/FactoryProdInputs/factory-prod-inputs-overview.component.ts
+++ src/app/Factory/FactoryProdInputs/factory-prod-inputs-overview.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-overview-cheese-input',
-  templateUrl: './factory-prod-inputs-search.component.html',
-  styleUrls: ['./factory-prod-inputs-search.component.css']
+  templateUrl: './factory-prod-inputs-overview.component.html',
+  styleUrls: ['./factory-prod-inputs-overview.component.css']
 })
 
 export class FactoryProdInputsOverview {
-
+  sendStatus = ():void => {
+    event.preventDefault()
+    console.log('testiong b utton')
+    // changeViewStatus('detailedView', filteredRecords.id);
+  }
 };
