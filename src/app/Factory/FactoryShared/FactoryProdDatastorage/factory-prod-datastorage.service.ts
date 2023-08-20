1639a6ced1ff22e7e5e19b04d39ce821264d82f9
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-20 08:44:09 -0600

    add-new-documentation-for-services

--- src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
+++ src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CheeseDataStructureModel } from "../CheeseDataStructure/cheese-data-structure.model";
 import { map } from "rxjs/operators";
-import { Firestore, collection, collectionData } from "@angular/fire/firestore";
+import { Firestore, collection, collectionData, doc, addDoc } from "@angular/fire/firestore";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
