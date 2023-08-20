2c9eaa9290e61fc06fa174533d02fdea4a306ae3
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-20 09:28:14 -0600

    create-update-logic

--- src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
+++ src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CheeseDataStructureModel } from "../CheeseDataStructure/cheese-data-structure.model";
 import { map } from "rxjs/operators";
-import { Firestore, collection, collectionData, doc, addDoc, deleteDoc } from "@angular/fire/firestore";
+import { Firestore, collection, collectionData, doc, addDoc, deleteDoc, updateDoc } from "@angular/fire/firestore";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
   constructor(private http: HttpClient, private firebase:Firestore) {}
 
   getCheeseRecords() {
-    let id = 'k8kYKXoJ5B2fjDnDh2bZ';
+    let id = 'YPUUeBo3JOynRvATDQ9u';
     // let notesCollection = collection(this.firebase, 'factory-chesse-records');
-    let notesCollection = collection(this.firebase, 'factory-chesse-records'+id);
-    return collectionData(notesCollection, {idField:'id'})
+
+    // setDoc(doc(db, "collectionName", userID, "anotherCollectionName", documentID), ...
+    // db.collection("app").document("users").collection(uid).document("notifications")
+    let notesCollection = collection(this.firebase, 'factory-chesse-records/')
+    const docRef = doc(this.firebase, "factory-chesse-records/", "yftq9RGp4jWNSyBZ1D6L");
+
+
+    // let notesCollection = collection(this.firebase, 'factory-chesse-records/','YPUUeBo3JOynRvATDQ9u');
+    return collectionData(notesCollection)
   }
 
   getSpecificCheeseRecord(id:string) {
@@ -22,9 +29,35 @@ export class DataStorageService {
   }
 
   storeCheeseRecord(cheeseData: CheeseDataStructureModel) {
-    this.http.post<CheeseDataStructureModel>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json', cheeseData)
-    .subscribe(resData => {
-      console.log(resData);
+    // this.http.post<CheeseDataStructureModel>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json', cheeseData)
+    // .subscribe(resData => {
+    //   console.log(resData);
+    // })
+  }
+
+  updateCheeseRecord() {
+    const docRef = doc(this.firebase, "factory-chesse-records/", "yftq9RGp4jWNSyBZ1D6L");
+
+    const data = {
+      cheeseName: 'Comiteco AA',
+      cheeseStatus: "Entrada",
+      cheeseAvailability: true,
+      piecesNo: 145,
+      piecesOutNo: 0,
+      weight: 0,
+      currentWeight: 0,
+      potsNo: 14,
+      dateIn: "2023-08-10",
+      dateOut: '',
+      price: 0,
+      approveProd: 'Verdadero',
+      loteNo: 2,
+      refrigerationType: "Chica"
+    };
+
+    updateDoc(docRef, data)
+    .then(docRef => {
+      console.log("A New Document Field has been added to an existing document");
     })
   }
 }
