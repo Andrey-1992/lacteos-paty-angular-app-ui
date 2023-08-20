aac79fe23a06dc59a6bd84454a9a932e4634e41e
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-20 09:00:36 -0600

    create-service-tocall-for-speific-id

--- src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
+++ src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
@@ -2,11 +2,40 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CheeseDataStructureModel } from "../CheeseDataStructure/cheese-data-structure.model";
 import { map } from "rxjs/operators";
-import { Firestore, collection, collectionData, doc, addDoc } from "@angular/fire/firestore";
+import { Firestore, collection, collectionData, doc, addDoc, deleteDoc } from "@angular/fire/firestore";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
   constructor(private http: HttpClient, private firebase:Firestore) {}
+
+  getCheeseRecords() {
+    let id = 'k8kYKXoJ5B2fjDnDh2bZ';
+    // let notesCollection = collection(this.firebase, 'factory-chesse-records');
+    let notesCollection = collection(this.firebase, 'factory-chesse-records'+id);
+    return collectionData(notesCollection, {idField:'id'})
+  }
+
+  getSpecificCheeseRecord(id:string) {
+    id = 'k8kYKXoJ5B2fjDnDh2bZ';
+    let notesCollection = collection(this.firebase, 'factory-chesse-records'+id);
+    return collectionData(notesCollection)
+  }
+
+  storeCheeseRecord(cheeseData: CheeseDataStructureModel) {
+    this.http.post<CheeseDataStructureModel>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json', cheeseData)
+    .subscribe(resData => {
+      console.log(resData);
+    })
+  }
+}
+
+
+
+
+
+
+
+
 
   // getCheeseRecords() {
   //   return this.http.get<CheeseDataStructureModel[]>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json/')
@@ -21,29 +50,3 @@ export class DataStorageService {
   //   //   console.log(resData);
   //   // })
   // }
-
-
-
-  getCheeseRecords() {
-    let notesCollection = collection(this.firebase, 'factory-chesse-records');
-    return collectionData(notesCollection)
-    // return this.http.get<CheeseDataStructureModel[]>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json/')
-    // .pipe(map(resData => {
-    //   const postArray = [];
-    //   for (const key in resData) {
-    //     postArray.push({...resData[key], id: key})
-    //   }
-    //   return postArray;
-    // }))
-    // .subscribe(resData => {
-    //   console.log(resData);
-    // })
-  }
-
-  storeCheeseRecord(cheeseData: CheeseDataStructureModel) {
-    this.http.post<CheeseDataStructureModel>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json', cheeseData)
-    .subscribe(resData => {
-      console.log(resData);
-    })
-  }
-}
