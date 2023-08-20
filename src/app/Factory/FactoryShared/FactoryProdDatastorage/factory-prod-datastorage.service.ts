a89e67856c328560394659398e8667eb949b0bb4
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-20 08:37:01 -0600

    implemeent-notes-co0llection-data-service

--- src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
+++ src/app/Factory/FactoryShared/FactoryProdDatastorage/factory-prod-datastorage.service.ts
@@ -2,20 +2,39 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CheeseDataStructureModel } from "../CheeseDataStructure/cheese-data-structure.model";
 import { map } from "rxjs/operators";
+import { Firestore, collection, collectionData } from "@angular/fire/firestore";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private firebase:Firestore) {}
+
+  // getCheeseRecords() {
+  //   return this.http.get<CheeseDataStructureModel[]>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json/')
+  //   .pipe(map(resData => {
+  //     const postArray = [];
+  //     for (const key in resData) {
+  //       postArray.push({...resData[key], id: key})
+  //     }
+  //     return postArray;
+  //   }))
+  //   // .subscribe(resData => {
+  //   //   console.log(resData);
+  //   // })
+  // }
+
+
 
   getCheeseRecords() {
-    return this.http.get<CheeseDataStructureModel[]>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json/')
-    .pipe(map(resData => {
-      const postArray = [];
-      for (const key in resData) {
-        postArray.push({...resData[key], id: key})
-      }
-      return postArray;
-    }))
+    let notesCollection = collection(this.firebase, 'factory-chesse-records');
+    return collectionData(notesCollection)
+    // return this.http.get<CheeseDataStructureModel[]>('https://fabrica-produccion-database-default-rtdb.firebaseio.com/cheese-record.json/')
+    // .pipe(map(resData => {
+    //   const postArray = [];
+    //   for (const key in resData) {
+    //     postArray.push({...resData[key], id: key})
+    //   }
+    //   return postArray;
+    // }))
     // .subscribe(resData => {
     //   console.log(resData);
     // })
