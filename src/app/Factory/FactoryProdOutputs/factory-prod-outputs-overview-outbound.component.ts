292927434074f1be5e598a08a18c0f17b3b741dd
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-08-26 10:17:12 -0600

    fix-comments-layout

--- src/app/Factory/FactoryProdOutputs/factory-prod-outputs-overview-outbound.component.ts
+++ src/app/Factory/FactoryProdOutputs/factory-prod-outputs-overview-outbound.component.ts
@@ -9,18 +9,37 @@ import { CheeseDataStructureModel } from '../FactoryShared/CheeseDataStructure/c
 
 export class FactoryProdOutputsOverviewOutbound implements OnInit{
   @Input() filteredRecords: CheeseDataStructureModel = {};
-  @Output() detailView = new EventEmitter<boolean>();
-  @Output() selectedRecord = new EventEmitter<CheeseDataStructureModel>();
-  cleanedDate:string = '';
+  // @Output() detailView = new EventEmitter<boolean>();
+  // @Output() selectedRecord = new EventEmitter<CheeseDataStructureModel>();
+  selectedCategory: string;
+  selectedRefrigeration: string;
+  cleanedDate:string;
+  selectedDate: string;
+  selectedDateIn: string;
+  selectedDateOut: string;
+  selectedApprovedProd: string;
+  selectedPiecesNo: number;
+  selectedPiecesOutNo: number;
+  selectedPotsNo: number;
+  selectedLoteNo: number;
+  selectedPrice: number;
+  selectedWeight: number;
+  piecesSubstraction: number
+  isDisable: boolean = false;
 
   ngOnInit(): void {
-    console.log(this.filteredRecords)
-    this.cleanedDate = this.filteredRecords.dateIn.slice(0, 10).split('-').join(' / ')
-  }
-
-  sendStatus = ():void => {
-    event.preventDefault()
-    this.detailView.emit(true);
-    this.selectedRecord.emit(this.filteredRecords);
+    this.cleanedDate = this.filteredRecords.dateIn.slice(0, 10).split('-').join(' / ');
+    this.selectedCategory = this.filteredRecords.cheeseName;
+    this.selectedRefrigeration = this.filteredRecords.refrigerationType;
+    this.selectedApprovedProd = this.filteredRecords.approveProd;
+    this.selectedDateIn = this.filteredRecords.dateIn;
+    this.selectedDateOut = this.filteredRecords.dateOut;
+    this.selectedPiecesNo = this.filteredRecords.piecesNo;
+    this.selectedPiecesOutNo = this.filteredRecords.piecesOutNo;
+    this.selectedPotsNo = this.filteredRecords.potsNo;
+    this.selectedLoteNo = this.filteredRecords.loteNo;
+    this.selectedPrice = this.filteredRecords.price;
+    this.selectedWeight = this.filteredRecords.weight;
+    // console.log(this.filteredRecords)
   }
 };
